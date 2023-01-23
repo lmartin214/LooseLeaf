3215339747b5666302685c4dec698c67bf0fe527
lmartin214@yahoo.com <you@example.com>
2023-01-23 12:03:29 -0800

    imported Router+ endpoint, apolloClient + instance, GlobalState + context

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+//importing various dependencies
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { 
+  ApolloClient, 
+  InMemoryCache, 
+  ApolloProvider, 
+  createHttpLink 
+  } 
+from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
-function App() {
+//importing various components
+import Home from './pages/Home';
+import Detail from './pages/Detail';
+import NoMatch from './pages/NoMatch';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Nav from './components/Nav';
+import { StoreProvider } from './utils/GlobalState';
+import Success from './pages/Success';
+import OrderHistory from './pages/OrderHistory';
+
+//creates an ApolloClient instance which is used to send requests to a GraphQL server
+const httpLink = createHttpLink({ 
+  uri: '/graphql',// used to define the endpoint of the GraphQL server
+});
+
+//used to attach an authorization token to the GraphQL request if one is present in localStorage
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+//sets up a new ApolloClient object with an authentication link and a HTTP link, as well as a new InMemoryCache object.
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+function App() { //setting up routes for the application
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+    <Router>
+      <div>
+        <StoreProvider>
+          <Nav />
+          <Routes>
+            <Route 
+              path="/" 
+              element={<Home />} 
+            />
+            <Route 
+              path="/login" 
+              element={<Login />} 
+            />
+            <Route 
+              path="/signup" 
+              element={<Signup />} 
+            />
+            <Route 
+              path="/success" 
+              element={<Success />} 
+            />
+            <Route 
+              path="/orderHistory" 
+              element={<OrderHistory />} 
+            />
+            <Route 
+              path="/products/:id" 
+              element={<Detail />} 
+            />
+            <Route 
+              path="*" 
+              element={<NoMatch />} 
+            />
+          </Routes>
+        </StoreProvider>
+      </div>
+    </Router>
+  </ApolloProvider>
   );
 }
 
