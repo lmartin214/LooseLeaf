d4c52f9034749f08b959fea1f2da926435992071
lmartin214@yahoo.com <you@example.com>
2023-01-23 12:39:48 -0800

    action to update state

---
+++ client/src/utils/actions.js
@@ -0,0 +1,19 @@
+// This is a collection of action types used to update the state in Redux.
+// This action type is used to update the products in the store.
+export const UPDATE_PRODUCTS = "UPDATE_PRODUCTS";
+// This action type is used to add a single product to the cart.
+export const ADD_TO_CART = "ADD_TO_CART";
+// This action type is used to add multiple products to the cart.
+export const ADD_MULTIPLE_TO_CART = "ADD_MULTIPLE_TO_CART";
+// This action type is used to remove a product from the cart.
+export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
+// This action type is used to clear the cart.
+export const CLEAR_CART = "CLEAR_CART";
+// This action type is used to update the quantity of a product in the cart.
+export const UPDATE_CART_QUANTITY = "UPDATE_CART_QUANTITY";
+// This action type is used to toggle the cart.
+export const TOGGLE_CART = "TOGGLE_CART";
+// This action type is used to update the categories in the store.
+export const UPDATE_CATEGORIES = "UPDATE_CATEGORIES";
+// This action type is used to update the current category.
+export const UPDATE_CURRENT_CATEGORY = "UPDATE_CURRENT_CATEGORY";
