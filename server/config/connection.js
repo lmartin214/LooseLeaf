6cd2d421c0e7c11158bf9f1d798ecee447311ef9
lmartin214@yahoo.com <you@example.com>
2023-01-24 08:39:53 -0800

    added mongoDB connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,10 @@
 //config using Mongoose 
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/LooseLeaf', { //Connection to Database
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/looseleaf',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+);
 
 module.exports = mongoose.connection;
